32c70c3b800013064c45fd733e7f693199cad454
danieldulaney <dulaney.daniel@gmail.com>
2015-10-15 23:24:38 -0400

    Example angular app working

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var app = angular.module('StarterApp', ['ngMaterial']);
+
+app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+  $scope.toggleSidenav = function(menuId) {
+    $mdSidenav(menuId).toggle();
+  };
+ 
+}]);
