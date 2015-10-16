cb6363f9d051e46bbfa73538669e22478690a392
danieldulaney <dulaney.daniel@gmail.com>
2015-10-16 02:48:47 -0400

    Added icon files and configurations

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,11 @@
 
 var app = angular.module('App', ['ngMaterial']);
 
+app.config(function($mdIconProvider){
+	$mdIconProvider
+		.iconSet('navigation','/img/icons/svg-sprite-navigation.svg');
+});
+
 app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
   $scope.toggleSidenav = function(menuId) {
     $mdSidenav(menuId).toggle();
