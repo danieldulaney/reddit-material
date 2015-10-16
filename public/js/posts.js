6a2f6bd5acda5b35c54291d3dba35e8050a5e63e
danieldulaney <dulaney.daniel@gmail.com>
2015-10-16 01:58:29 -0400

    Added rm-post directive

--- public/js/posts.js
+++ public/js/posts.js
@@ -6,18 +6,26 @@ var posts = angular.module('Posts', ['App', 'ngMaterial', 'ngResource']);
 posts.controller('PostsDataCtrl', ['$resource', function($resource){
 	var ctrl = this;
 
-	ctrl.posts = {posts: 'yup'};
+	ctrl.posts = [];
 
 	ctrl.PostResource = $resource('https://api.reddit.com/r/:subreddit',
 		{subreddit: '@r'}
 	);
 
-	ctrl.PostResource.get({subreddit: 'funny'}, function(posts){
+	ctrl.PostResource.get({subreddit: 'polandball'}, function(posts){
 		ctrl.posts = posts.data.children;
 	});
 }]); // PostsDataCtrl
 
-
+posts.directive('rmPost', function(){
+	return {
+		restrict: 'E',
+		templateUrl: '/templates/post.html',
+		scope: {
+			post: '=post',
+		},
+	};
+});
 
 
 })(); // Encapsulation
