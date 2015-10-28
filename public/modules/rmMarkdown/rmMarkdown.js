da1c61c0c4aeb7417f249de40c6797ade11a2dea
danieldulaney <dulaney.daniel@gmail.com>
2015-10-27 22:08:56 -0400

    More markdown parsing

--- public/modules/rmMarkdown/rmMarkdown.js
+++ public/modules/rmMarkdown/rmMarkdown.js
@@ -1,7 +1,7 @@
 (function(){
 	'use strict';
 
-	var rmMarkdown = angular.module('rmMarkdown', []);
+	var rmMarkdown = angular.module('rmMarkdown', ['ngSanitize']);
 
 	rmMarkdown.directive('rmMarkdown', [function(){
 		return {
@@ -9,28 +9,48 @@
 			scope: {
 				content: '=',
 			},
-			controller: ['$scope', function($scope){
+			controller: ['$scope', '$sce', function($scope, $sce){
 				$scope.parse = function(string){
-					// Begin by sanitizing string (remove any existing HTML)
 
-					console.log('String: ' + string);
+					// Begin by sanitizing string (remove any existing HTML)
 					var escMap = {
-						'&': '&amp;',
 						'<': '&lt;',
 						'>': '&gt',
 						'"': '&quot',
 						'\'': '&#39;',
 						'/': '&#x2F;',
+						'\\*': '&#042;',
+						'\\_': '&#095;',
+						'\\~': '&#126;',
+						'\\^': '&#094;',
+						'\\|': '&#124;',
+						'\\:': '&#058;',
+						'\\=': '&#061;',
+						'\\-': '&#045;',
 					};
-					string = string.replace(/[&<>"'/]/g, function(match){return escMap[match];});
-					console.log('Sanitized: ' + string);
 
-					// Raw links
-					string = string.replace(/(http:\/\/|www\.)[\w\.]+/ig, function(match){
+					// Sanitize string and handle escape characters
+					string = string.replace(/[<>"'/]|\\\*|\\_|\\~|\\^|\\\||\\:|\\=|\\-/g, function(match){return escMap[match];});
+
+
+					// Raw links (http://whatever)
+					string = string.replace(/http:\/\/[\w\.]+/ig, function(match){
 						return '<a href="' + match + '">' + match + '</a>';
 					});
 
-					return string;
+					// Raw links (www.whatever)
+					string = string.replace(/www\.[\w\.]+/ig, function(match){
+						return '<a href="http://' + match + '">' + match + '</a>';
+					});
+
+					// Formatted links [whatever](whatever.com)
+					string = string.replace(/\[.*\]\s*(http.*)/ig, function(match){
+						return 'flink';
+					});
+
+					// Horizontal lines
+
+					return $sce.trustAs('html', string);
 				};
 			}],
 			template: '<div style="overflow:wrap" ng-bind-html="parse(content)"></div>',
