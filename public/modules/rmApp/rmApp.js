65230b2753764a4ba51e18ad2c442e3c66f0cb1d
danieldulaney <dulaney.daniel@gmail.com>
2015-10-29 15:59:33 -0400

    More changes to style and comment structure

--- public/modules/rmApp/rmApp.js
+++ public/modules/rmApp/rmApp.js
@@ -4,13 +4,16 @@
 var app = angular.module('rmApp', ['ngMaterial', 'rmDisplay', 'rmData']);
 
 app.config(function($mdIconProvider){
-	$mdIconProvider.iconSet('navigation','/img/icons/svg-sprite-navigation.svg')
+	$mdIconProvider
+		.iconSet('navigation','/img/icons/svg-sprite-navigation.svg')
+		.iconSet('action', '/img/icons/svg-sprite-action.svg')
+		.iconSet('content', '/img/icons/svg-sprite-content.svg')
 		.icon('reddit:downvote', '/img/icons/reddit-downvote.svg')
 		.icon('reddit:upvote', '/img/icons/reddit-upvote.svg')
 		;
 });
 
-app.controller('AppCtrl', ['$scope', '$mdSidenav', '$mdIcon', function($scope, $mdSidenav, $mdIcon){
+app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
 	$scope.toggleSidenav = function(menuId) {
 		$mdSidenav(menuId).toggle();
 	};
